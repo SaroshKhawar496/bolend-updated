4b9c3e3539ebed9ee3e0a34fb4a1baefba4e384d
DL <dvdgege5@gmail.com>
2018-10-10 19:41:48 -0400

    add router and route reuse-strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+// utils 
+import { CustomReuseStrategy } from './reuse-strategy';
+import { 
+	// ActivatedRoute, 
+	RouterModule, 
+	RouteReuseStrategy ,
+	// DefaultUrlSerializer, ActivatedRouteSnapshot, DetachedRouteHandle
+} from '@angular/router';
+
+
+// components
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './main/dashboard/dashboard.component';
+
+
+
+// app router
+const appRoutes = [
+	{ path: '', component: DashboardComponent }
+];
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		DashboardComponent
+	],
+	imports: [
+		BrowserModule,
+		RouterModule.forRoot( appRoutes )
+	],
+	providers: [{
+		provide: RouteReuseStrategy, 
+		useClass: CustomReuseStrategy
+	}],
+	bootstrap: [
+		AppComponent
+	]
 })
 export class AppModule { }
