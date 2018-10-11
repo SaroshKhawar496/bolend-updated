2cfc9d67b31ea9d89cd2d0a56d951c1b9a1ea26b
DL <dvdgege5@gmail.com>
2018-10-11 00:28:26 -0400

    add sidebar navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'lendapp';
+	title = '444chan.';
+	sidenavHidden = true;
+
+	constructor() {
+
+	}
+
+
+
+	// event handlers
+
+	public toggleSidebar ( hide : boolean = undefined ) : void {
+		if ( hide != undefined )
+			this.sidenavHidden = hide;
+		else
+			this.sidenavHidden = !this.sidenavHidden;
+	}
 }
