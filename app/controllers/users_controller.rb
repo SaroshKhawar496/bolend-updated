99efec99c0cf3d581f95f48c7a13899d6f748324
SaroshKhawar <sarosh.khawar@mail.utoronto.ca>
2018-10-19 19:12:45 -0400

    Disabled the require_same_user method in Application Controller, wasn't working correctly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 	
 	#preventing a user to goto other user profile  by changing URI
-	before_action :require_same_user, only: [:show]
+	# before_action :require_same_user, only: [:show]
 
 	def new
 		@user = User.new
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user.inspect
   end
 
 	def create
@@ -29,14 +30,14 @@ class UsersController < ApplicationController
 
 		end
 
-	def require_same_user 
-		#if current user doesn't match the visiting profile user
-		#then shows error and takes you back to root_path
-		if current_user != @user
-			flash[:notice] = "You cannot see other person's proifle!"
-			redirect_to root_path
-		end
+	# def require_same_user 
+	# 	#if current user doesn't match the visiting profile user
+	# 	#then shows error and takes you back to root_path
+	# 	if current_user != @user
+	# 		flash[:notice] = "You cannot see other person's proifle!"
+	# 		redirect_to root_path
+	# 	end
 
-	end
+	# end
 	
 end
