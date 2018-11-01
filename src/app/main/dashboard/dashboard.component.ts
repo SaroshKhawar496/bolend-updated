6834a8289522ec201f1d610a9b4958deb6bf3b89
DL <dvdgege5@gmail.com>
2018-11-01 13:48:34 -0100

    implement ReST toy example that returns list of items in homepage

--- src/app/main/dashboard/dashboard.component.ts
+++ src/app/main/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../http.service';
 
 @Component({
 	selector: 'app-dashboard',
@@ -8,14 +9,27 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
 	name: string;
+	itemList: any;
 
 
 	constructor (
-
+		private http: HttpService
 	) { }
 
 	ngOnInit() {
 		this.name = 'anon';
+		this.getItemsList();
+	}
+
+
+	getItemsList () : void {
+		let path: string = '/items.json';
+		this.http.getObservable (path).subscribe (
+			data => {
+				this.itemList = data;
+				console.log ('getItemsList', data);
+			}
+		)
 	}
 
 }
