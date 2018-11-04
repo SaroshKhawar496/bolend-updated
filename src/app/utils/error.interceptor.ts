859472424c660c4d818f1fb8d0f4ae8949f3758d
DL <dvdgege5@gmail.com>
2018-11-03 20:15:50 -0400

    make http error interceptor throw a more detailed error when intercepted

--- src/app/utils/error.interceptor.ts
+++ src/app/utils/error.interceptor.ts
@@ -25,7 +25,8 @@ export class ErrorInterceptor implements HttpInterceptor {
 				location.reload(true);
 			}
 			
-			const error = err.error.message || err.statusText;
+			// const error = err.error.message || err.statusText;
+			const error = err;
 			return throwError(error);
 		}));
 	}
