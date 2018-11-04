82e20e3c13a12b85538a2d1b8af3f2c8a0f0b5b9
DL <dvdgege5@gmail.com>
2018-11-04 00:20:46 -0400

    get data about arbitrary user (i.e. does not have to be current_user)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
 	
 	#current_user is available through devise gem
   def show
-    @user = User.find(current_user.id)
+    # @user = User.find(current_user.id)
+    @user = User.find(params[:id])
   end
 
 
