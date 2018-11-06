a800bb6cc44d4fec0a9d07f0f358ed344a683fda
shethpa1 <pal.sheth@mail.utoronto.ca>
2018-11-06 15:20:31 -0500

    added json object for frontend handling

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,24 +1,38 @@
 class NotificationsController < ApplicationController
 
 	def show
-		@items = Item.where(user_id: current_user.id) #find all items owned by current user
-		puts "current user id is #{current_user.id}"
-		json_obj = {}
+		@items = Item.where(user_id: current_user.id) #find all items owned by current user, return a collection of items
+		
+		json_str = "{\n\t\"notifications\":\n\t[\n" # json string builder, create notications with an array of notifs
+
 		@items.each do |curr|
-			#puts "#{curr.name}"
-			@request = Request.where(item_id: curr.id) # find all requests on items owned by current user
+			@request = Request.where(item_id: curr.id) # find all requests on items owned by current use
 			@request.each do |req|
 				@users = User.find(req.user_id)
 				fname = @users.fname
 				lname = @users.lname
 				item_name = curr.name
-				puts "#{fname} #{lname} wants to borrow #{item_name}"	
 
-				json_obj ("firstname" => fname, "lastname" => lname, "itemname" => item_name)
+				json_str += "\t\t{firstname\": #{fname}, \"lastname\": #{lname}, \"itemname\": #{item_name}},\n"
 
 			end
 		end 
-
-		puts "#{json_obj}"
+		json_str = json_str[0...-2]
+		json_str += "\n\t]\n}"
+		puts "#{json_str}"
+		
 	end
 end
+
+# -----------------------------------JSON TEMPLATE-----------------------------------
+# {
+# 	"notifications":
+# 	[
+# 		{"firstname": "#{fname}", "lastname": "#{lname}", "itemname": "#{item_name}"},
+# 		{"firstname": "#{fname}", "lastname": "#{lname}", "itemname": "#{item_name}"},
+# 		...
+# 		...
+# 		...
+# 		{"firstname": "#{fname}", "lastname": "#{lname}", "itemname": "#{item_name}"}
+# 	]
+# }
