627561424f5736929fc864d3f261f2a90312fa46
DL <dvdgege5@gmail.com>
2018-11-06 15:46:47 -0500

    add error checks and feedback to password recovery

--- src/app/accounts/recover/recover.component.ts
+++ src/app/accounts/recover/recover.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
 	selector: 'app-recover',
@@ -21,8 +22,15 @@ export class RecoverComponent extends LoginComponent {
 		let path: string = "/accounts/password";
 		let payload: object = { user: { email: this.email } };
 		this.http.postObservable ( path, payload ).subscribe (
-			res => {
+			res => {		// success
 				console.log ('submitRecover()', res );
+				this.alert.success ( "An email has been sent to you, containing a link to reset your password" );
+			},
+			(err: HttpErrorResponse) => {
+				console.error(err);
+				if ( err.status == 422 ) {
+					this.alert.error ( "The email you entered does not exist. Please try again" );
+				}
 			}
 		)
 	}
