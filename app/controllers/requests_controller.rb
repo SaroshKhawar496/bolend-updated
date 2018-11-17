dc47375b614857e8abf025310695356fde6f42be
muhammadammaraamir <muhammad.aamir@mail.utoronto.ca>
2018-11-17 03:20:26 -0500

    Fixed 500 error when requesting item. Also created show view for request which is rendered upon successful request creation

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,36 +3,16 @@ class RequestsController < ApplicationController
 
   def create
     @item = Item.find(params[:id])
-    if ! current_user.requested_items.include? @item #to prevent user from requesting the same item again
-        if ! current_user.items.include? @item #to prevent user from requesting his own item
-          @request = current_user.requests.new(item: @item) 
-          if @request.save
+    if current_user.requested_items.include? @item #to prevent user from requesting the same item again
+      render json: {"message": "You have already requested this item"}, status: :unprocessable_entity
+    elsif current_user.items.include? @item #to prevent user from requesting his own item
+      render json: {"message": "You cannot request an item that belongs to you"}, status: :unprocessable_entity
+    else
+      @request = current_user.requests.new(item: @item) 
+      if @request.save
 
-            #creating the notification
-
-            #store the user id of the requested_item owner in the notification user id
-            item_owner_id = @item.user_id
-
-            puts "\n\n item_owner_id:#{item_owner_id} \n\n"
-            #store the item name and the requesting user name in the description of the notification
-            detail = "#{current_user.fname} requested your #{@item.name}"
-
-            puts "\n\n Desc:#{detail} \n\n"
-
-            @notification = Notification.new(user_id: item_owner_id, description: detail)
-            if @notification.save
-              # render json: {
-              #   "message": "Request Notification has been created."
-              # }
-            else
-              render json: @notification.errors
-            
-            end
-
-            #redirect_to item_path(@item)
-            render :index, status: :ok
-          end
-        end
+        render :show, status: :ok, location: @request
+      end
     end    
   end
 
@@ -40,8 +20,11 @@ class RequestsController < ApplicationController
     @user = User.find(current_user.id)
   end
 
+  def show
+    
+  end
+
   def destroy
-    @request = Request.find(params[:id])
     @request.destroy
   end
 
---
+++ app/views/requests/show.json.jbuilder
@@ -0,0 +1,3 @@
+json.request @request
+json.requested_item @request.item
+json.item_owner @request.item.user
