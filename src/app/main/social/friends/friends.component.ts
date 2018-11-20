91197c6c93cbfb0b1026b754739eb3fb867f9c7f
DL <dvdgege5@gmail.com>
2018-11-20 15:39:37 -0500

    switch friend requests and requests received

--- src/app/main/social/friends/friends.component.ts
+++ src/app/main/social/friends/friends.component.ts
@@ -107,12 +107,12 @@ export const FriendTabs: {
 	},
 	incoming: {
 		title: 'Requests Received', 
-		apiRoute: 'requested',
+		apiRoute: 'get-pending',
 		controls: [ FriendControls.accept, FriendControls.deny, FriendControls.block ]
 	},
 	outgoing: {
 		title: 'Requests Sent', 
-		apiRoute: 'get-pending',
+		apiRoute: 'requested',
 		controls: [ FriendControls.profile, FriendControls.block ]
 	},
 	discover: { 
