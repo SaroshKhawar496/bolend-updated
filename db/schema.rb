965f354d0d19082aa3a4328d8bb964b8054bab09
shethpa1 <pal.sheth@mail.utoronto.ca>
2018-11-27 15:02:41 -0500

    added privilege function to check user privilege rights, added privateMode flag in user table, fixed blocked users bug, and added feature to cancel friend request.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_063038) do
+ActiveRecord::Schema.define(version: 2018_11_27_072220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,6 +116,13 @@ ActiveRecord::Schema.define(version: 2018_11_26_063038) do
     t.index ["name"], name: "index_hashtags_name"
   end
 
+  create_table "user_relations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_relations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "fname"
     t.string "lname"
@@ -130,10 +137,7 @@ ActiveRecord::Schema.define(version: 2018_11_26_063038) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
+    t.boolean "privateMode", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
