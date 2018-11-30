07889f45b8a0148c2682a0fb6641810dc0d38932
Nik Tsytsarkin <nik.tsytsarkin@mail.utoronto.ca>
2018-11-30 13:02:09 -0800

    adder tests for mailer

--- test/mailers/notification_mailer_test.rb
+++ test/mailers/notification_mailer_test.rb
@@ -2,11 +2,24 @@ require 'test_helper'
 
 class NotificationMailerTest < ActionMailer::TestCase
   test "item_request_email" do
-    mail = NotificationMailer.item_request_email
-    assert_equal "Item request email", mail.subject
+    recepient = User.new
+    recepient.email = "to@example.org"
+    recepient.fname = "Recepient"
+    sender = User.new
+    sender.fname = "sender"
+    item = Item.new
+    item.name = "Test Item"
+    mail = NotificationMailer.item_request_email([recepient, sender, item])
+    assert_equal "Your Item Has Been Requested", mail.subject
     assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["borrowland000@gmail.com"], mail.from
     assert_match "Hi", mail.body.encoded
+    assert_match "has requested to borrow your", mail.body.encoded
+    assert_match "See The Request", mail.body.encoded
+    assert_match "Click on the link below to see:", mail.body.encoded
+    assert_match "Test Item", mail.body.encoded
+    assert_match "Sender", mail.body.encoded
+    assert_match "Recepient", mail.body.encoded
   end
 
 end
