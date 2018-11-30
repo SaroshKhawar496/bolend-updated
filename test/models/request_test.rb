b3ee03e6e0ca3bb52cfcc13b6f32356474d679c3
Nik Tsytsarkin <nik.tsytsarkin@mail.utoronto.ca>
2018-11-30 13:24:38 -0800

    tests added for requests

--- test/models/request_test.rb
+++ test/models/request_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class RequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "request exists" do
+    assert requests(:book_request)
+  end
+
+  test "request has a user" do
+    assert_not_nil requests(:book_request).user_id
+  end
+
+  test "request has an item" do
+    assert_not_nil requests(:book_request).item_id
+  end
+
+  test "request has created_at timestamp" do
+    assert_not_nil requests(:book_request).created_at
+  end
+
+  test "request has updated_at timestamp" do
+    assert_not_nil requests(:book_request).updated_at
+  end
+
+  test "request has days" do
+    assert_not_nil requests(:book_request).days
+  end
+
+  test "request days is 10" do
+    assert_equal(requests(:book_request).days, 10)
+  end
+
+  test "request compare status" do
+    assert_equal(requests(:book_request).status, 'pending')
+  end
+
 end
