7a20407fcda4c52f3af6b6e44394a7ac6d2ec0a1
Nik Tsytsarkin <nik.tsytsarkin@mail.utoronto.ca>
2018-11-30 12:42:10 -0800

    loan tests added

--- test/models/loan_test.rb
+++ test/models/loan_test.rb
@@ -16,4 +16,16 @@ class LoanTest < ActiveSupport::TestCase
   test "loan has created_at timestamp" do
     assert_not_nil loans(:book_loan).created_at
   end
+
+  test "loan has updated_at timestamp" do
+    assert_not_nil loans(:book_loan).updated_at
+  end
+
+  test "loan has duedate timestamp" do
+    assert_not_nil loans(:book_loan).duedate
+  end
+
+  test "loan compare duedate" do
+    assert_equal(loans(:book_loan).duedate, '2018-11-30 20:40:06.029348')
+  end
 end
