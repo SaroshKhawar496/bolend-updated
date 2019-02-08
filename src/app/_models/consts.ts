bc9651dfdc4d1872f74bdc63c0e69b1601e9a33b
SaroshKhawar496 <sarosh.496@gmail.com>
2019-02-07 23:36:41 -0500

    Changed the server error message

--- src/app/_models/consts.ts
+++ src/app/_models/consts.ts
@@ -1,3 +1,3 @@
 export class Consts {
-	static serverFaultMsg: string = 'OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!';
+	static serverFaultMsg: string = 'Server Error! Please try again later';
 }
